e2238d88f8d769a5a03ac16cfca9075ec939380c
Brandon Samaroo <samaroo.bran@gmail.com>
2022-04-24 19:37:55 -0400

    added coindata route

--- controllers/index.js
+++ controllers/index.js
@@ -13,7 +13,25 @@ exports.allCoins_get = (req, res, next) =>{
 			"x-access-token": `coinrankinga10ae712cc7e766112b2cfc7f72a88480535ea9e834717c3`,
 		}
 	}).then((result) => {
-		console.log(result)
+		return res.json(result.data)
+	}).catch((err) => {
+		console.log(err)
+		return res.json({"error": err})
+	});
+}
+
+
+exports.coinData_get = (req, res, next) =>{
+	console.log(req.body)
+	axios({
+		method: 'get',
+		url: `https://api.coinranking.com/v2/coins?timePeriod=${req.body.time}&symbols[]=${req.body.symbol}`,
+		withCredentials: true,
+		headers:{
+			"x-access-token": `coinrankinga10ae712cc7e766112b2cfc7f72a88480535ea9e834717c3`,
+		}
+	}).then((result) => {
+		console.log(result.data)
 		return res.json(result.data)
 	}).catch((err) => {
 		console.log(err)
